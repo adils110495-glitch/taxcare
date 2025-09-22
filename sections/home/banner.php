fce122316748fcf6a202f61a58d9216dde44ac76
Adil <adils110495@gmail.com>
2025-09-22 17:11:46 +0530

    added content to banner

--- sections/home/banner.php
+++ sections/home/banner.php
@@ -4,13 +4,11 @@
                 <div class="banner swiper-item" data-v-0bb9f87f="">
                   <div class="container" data-v-0bb9f87f="">
                     <div class="banner-content" data-v-0bb9f87f="">
-                      <h1 class="title mTitle" data-v-0bb9f87f=""><span data-v-0bb9f87f="">Build Interactive Real-Time
-                          Apps ____ </span>
-                        <p data-v-0bb9f87f=""><span data-v-0bb9f87f="">Voice , Video & ____ API </span></p>
+                      <h1 class="title mTitle" data-v-0bb9f87f=""><span data-v-0bb9f87f="">Simplify Income Tax Return Filing with </span>
+                        <p data-v-0bb9f87f=""><span data-v-0bb9f87f="">Expert Guidance & Smart Technology</span></p>
                       </h1>
-                      <p class="text" data-v-0bb9f87f="">Real-time SDKs for voice, _____, chat, live streaming, and
-                        __-_______ engagement. </p>
-                      <div class="start-building btn" data-v-0bb9f87f="">Start building </div>
+                      <p class="text" data-v-0bb9f87f="">Smart tax solutions for ITR filing, TDS returns, and expert consultations — anytime, anywhere. </p>
+                      <div class="start-building btn" data-v-0bb9f87f="">Simplify Your Taxes Now </div>
                     </div> <img src="_nuxt/img/img_index_banner.03ccd64.png" alt="png" class="banner-img"
                       data-v-0bb9f87f="" />
                   </div>
